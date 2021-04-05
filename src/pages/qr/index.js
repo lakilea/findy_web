0a5d06f6c8a7c49c52eefb7f97d615e626f110bb
Lakilea <zaferk.86@gmail.com>
2021-04-05 18:19:14 +0100

    added chat with qr owner button

--- src/pages/qr/index.js
+++ src/pages/qr/index.js
@@ -35,63 +35,68 @@ export default function QrScreen() {
     </div>
     :
     state.selectedItems.length > 0 ? 
-    <div className="container">
-      <div className="header">
-        <div className="headerText">
-          <img src={logo}></img>
+    <div>
+      <div className="container">
+        <div className="header">
+          <div className="headerText">
+            <img src={logo}></img>
+          </div>
+        </div>
+        <div className="body">
+          <div className="qr-desc">
+            {state.qrDescription}
+          </div>
+          <div className="qr-item-list">
+            {state.selectedItems.map((item) => 
+              {
+                let text = "";
+                let icon = faCoffee;
+
+                if (item.type==="address") {
+                  text = item.address + " " + item.country;
+                  icon = faLocationArrow;
+
+                  return (<div className="qr-item" key={item.id}>
+                    <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"https://maps.google.com/?q="+text} target="_blank">{text}</a>
+                  </div>)
+                }
+
+                if (item.type==="email") {
+                  text = item.emailAddress;
+                  icon = faEnvelope;
+
+                  return (<div className="qr-item" key={item.id}>
+                    <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"mailto:"+text}>{text}</a>
+                  </div>)
+                }
+
+                if (item.type==="phone") {
+                  text = item.phoneNumber;
+                  icon = faPhone;
+
+                  return (<div className="qr-item" key={item.id}>
+                    <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"tel:"+text}>{text}</a>
+                  </div>)
+                }
+
+                if (item.type==="social") {
+                  text = item.platform+"/"+item.socialAccount;
+                  if (item.platform==="facebook.com")
+                    icon = faFacebook;
+                  if (item.platform==="twitter.com")
+                    icon = faTwitter;
+
+                  return (<div className="qr-item" key={item.id}>
+                    <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"http://"+text} target="_blank">{text}</a>
+                  </div>)
+                }
+              }
+            )}
+          </div>
         </div>
       </div>
-      <div className="body">
-        <div className="qr-desc">
-          {state.qrDescription}
-        </div>
-        <div className="qr-item-list">
-          {state.selectedItems.map((item) => 
-            {
-              let text = "";
-              let icon = faCoffee;
-
-              if (item.type==="address") {
-                text = item.address + " " + item.country;
-                icon = faLocationArrow;
-
-                return (<div className="qr-item" key={item.id}>
-                  <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"https://maps.google.com/?q="+text} target="_blank">{text}</a>
-                </div>)
-              }
-
-              if (item.type==="email") {
-                text = item.emailAddress;
-                icon = faEnvelope;
-
-                return (<div className="qr-item" key={item.id}>
-                  <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"mailto:"+text}>{text}</a>
-                </div>)
-              }
-
-              if (item.type==="phone") {
-                text = item.phoneNumber;
-                icon = faPhone;
-
-                return (<div className="qr-item" key={item.id}>
-                  <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"tel:"+text}>{text}</a>
-                </div>)
-              }
-
-              if (item.type==="social") {
-                text = item.platform+"/"+item.socialAccount;
-                if (item.platform==="facebook.com")
-                  icon = faFacebook;
-                if (item.platform==="twitter.com")
-                  icon = faTwitter;
-
-                return (<div className="qr-item" key={item.id}>
-                  <FontAwesomeIcon icon={icon} color="#f69833" /> <a href={"http://"+text} target="_blank">{text}</a>
-                </div>)
-              }
-            }
-          )}
-        </div>
+      <div className="chat-container">
+        <a href="https://play.google.com/store/apps/details?id=com.croone.findy" className="chat-button" target="_blank">Chat with QR owner</a>
       </div>
     </div>
     :
