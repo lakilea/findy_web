ed0cc294040b3a4fa8a58b71f2e76b985ddba110
Lakilea <zaferk.86@gmail.com>
2021-05-02 00:21:06 +0100

    added keyring and paypal has been finished

--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 export const products = [
   {
     "code" : "1",
-    "name" : "Luggage tag",
+    "name" : "findy luggage tag",
     "maxQuantity":10,
     "images" : [
       require("./assets/products/1/luggage-tag-1.jpg"),
@@ -9,7 +9,21 @@ export const products = [
       require("./assets/products/1/luggage-tag-4.jpg"),
       require("./assets/products/1/luggage-tag-5.jpg")
     ],
-    "description" : <div>MDF Luggage Tag hardboard wooden. <br/>Size:10x7cm</div>,
+    "description" : <div>MDF Luggage Tag hardboard wooden. <br/>Size:10x7cm<br/>One side printed</div>,
+    "price" : "9.90"
+  },
+  {
+    "code" : "2",
+    "name" : "findy keyring",
+    "maxQuantity":10,
+    "images" : [
+      require("./assets/products/2/keyring-1.jpg"),
+      require("./assets/products/2/keyring-2.jpg"),
+      require("./assets/products/2/keyring-3.jpg"),
+      require("./assets/products/2/keyring-4.jpg"),
+      require("./assets/products/2/keyring-5.jpg")
+    ],
+    "description" : <div>MDF Keyring hardboard wooden. <br/>Size:3.5x3.5cm<br/>Double side printed</div>,
     "price" : "9.90"
   }
 ]
